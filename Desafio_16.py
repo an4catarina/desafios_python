1917f54a6168fc74ba835e5fa5aa05154f97e39c
Ana Catarina Santos <an4catarina@gmail.com>
2021-11-08 21:00:34 -0300

    1040 - Média 3

---
+++ Desafio_16.py
@@ -0,0 +1,28 @@
+N1, N2, N3, N4 = input().split()
+
+N1 = float(N1)
+N2 = float(N2)
+N3 = float(N3)
+N4 = float(N4)
+
+Media = (N1*2+N2*3+N3*4+N4)/10
+print(f"Media: {Media:.1f}")
+
+if Media >= 7.0:
+    print("Aluno aprovado.")
+
+if Media < 5.0:
+    print("Aluno reprovado.")
+    
+else:
+    print('Aluno em exame.')
+    NE = float(input())
+    print(f"Nota do exame: {NE}.")
+    Media2 = (Media+NE)/2
+    
+    if Media2 >= 5.0:
+        print("Aluno aprovado.")
+        print(f"Média final: {Media2:.1f}.")
+    else:
+        print("Aluno reprovado.")
+        print(f"Média final: {Media2:.1f}.")
