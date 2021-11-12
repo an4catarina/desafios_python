7c43eb714662bc082ef70c0f5aa915d6052a4b48
Ana Catarina Santos <an4catarina@gmail.com>
2021-11-12 15:52:25 -0300

    1180 - Menor e Posição

---
+++ Desafio_31.py
@@ -0,0 +1,7 @@
+Tamanho = int(input())
+valores = list(map(int, input().split()))
+
+minimo = min(valores)
+posicao = valores.index(minimo)
+print(f'Menor valor: {minimo}')
+print(f'Posicao: {posicao}')
